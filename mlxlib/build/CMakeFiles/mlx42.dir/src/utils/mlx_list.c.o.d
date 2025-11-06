1cf68bb57e3e24a5c2260ebd3350b0c49d904ce9
Wint Shwe Yi Oo <wintshweyioo77@gmail.com>
2025-11-06 15:18:37 +0700

    Add files via upload

---
+++ mlxlib/build/CMakeFiles/mlx42.dir/src/utils/mlx_list.c.o.d
@@ -0,0 +1,44 @@
+CMakeFiles/mlx42.dir/src/utils/mlx_list.c.o: \
+  /home/wintoo/42/MS\ 2/So\ Long/1/mlxlib/src/utils/mlx_list.c \
+  /home/wintoo/42/MS\ 2/So\ Long/1/mlxlib/include/MLX42/MLX42_Int.h \
+  /home/wintoo/42/MS\ 2/So\ Long/1/mlxlib/include/MLX42/MLX42.h \
+  /usr/lib/llvm-15/lib/clang/15.0.7/include/stddef.h \
+  /usr/lib/llvm-15/lib/clang/15.0.7/include/__stddef_max_align_t.h \
+  /usr/lib/llvm-15/lib/clang/15.0.7/include/stdint.h \
+  /usr/include/stdint.h \
+  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+  /usr/include/features.h /usr/include/features-time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+  /usr/include/x86_64-linux-gnu/bits/timesize.h \
+  /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+  /usr/include/x86_64-linux-gnu/bits/long-double.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+  /usr/include/x86_64-linux-gnu/bits/types.h \
+  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+  /usr/include/x86_64-linux-gnu/bits/time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wchar.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+  /usr/lib/llvm-15/lib/clang/15.0.7/include/stdbool.h \
+  /home/wintoo/42/MS\ 2/So\ Long/1/mlxlib/include/lodepng/lodepng.h \
+  /usr/include/string.h \
+  /home/wintoo/42/MS\ 2/So\ Long/1/mlxlib/include/glad/glad.h \
+  /home/wintoo/42/MS\ 2/So\ Long/1/mlxlib/include/KHR/khrplatform.h \
+  /usr/include/GLFW/glfw3.h /usr/include/stdlib.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+  /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+  /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+  /usr/include/memory.h /usr/include/stdio.h \
+  /usr/lib/llvm-15/lib/clang/15.0.7/include/stdarg.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+  /usr/include/x86_64-linux-gnu/bits/stdio.h /usr/include/linux/limits.h \
+  /usr/include/ctype.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+  /usr/include/x86_64-linux-gnu/bits/endianness.h /usr/include/assert.h
